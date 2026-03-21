c8193138021ae8507891123c79cad8063e33172f
Romeo-Martial <104857205+Romeo-Martial@users.noreply.github.com>
2026-03-21 23:16:37 +0100

    Add the file product.js to the code base
    the file implements the class Product which enforces the product rules

---
+++ src/features/catalog/domain/product.js
@@ -0,0 +1,77 @@
+import { ProductId } from "./valueObjects/productId";
+import { ProductName } from "./valueObjects/productName";
+import { ProductImageUrl } from "./valueObjects/productImageUrl";
+import { ProductCategory } from "./valueObjects/productCategory";
+import { Money } from "../../_shared/domain/valueObjects/money";
+
+export class Product {
+  constructor({ id, name, price, imageUrl, category }) {
+    if (!(id instanceof ProductId)) {
+      throw new Error("id must be of type ProductId");
+    }
+
+    if (!(name instanceof ProductName)) {
+      throw new Error("name must be of type ProductName");
+    }
+
+    if (!(price instanceof Money)) {
+      throw new Error("price must be of type Money");
+    }
+
+    if (!(imageUrl instanceof ProductImageUrl)) {
+      throw new Error("imageUrl must be of type ProductImageUrl");
+    }
+
+    if (!(category instanceof ProductCategory)) {
+      throw new Error("category must be of type ProductCategory");
+    }
+
+    this.id = id;
+    this.name = name;
+    this.price = price;
+    this.imageUrl = imageUrl;
+    this.category = category;
+
+    Object.freeze(this);
+  }
+
+  rename(newName) {
+    return new Product({
+      id: this.id,
+      name: newName,
+      price: this.price,
+      imageUrl: this.imageUrl,
+      category: this.category,
+    });
+  }
+
+  changePrice(newPrice) {
+    return new Product({
+      id: this.id,
+      name: this.name,
+      price: newPrice,
+      imageUrl: this.imageUrl,
+      category: this.category,
+    });
+  }
+
+  changeImage(newImageUrl) {
+    return new Product({
+      id: this.id,
+      name: this.name,
+      price: this.price,
+      imageUrl: newImageUrl,
+      category: this.category,
+    });
+  }
+
+  changeCategory(newCategory) {
+    return new Product({
+      id: this.id,
+      name: this.name,
+      price: this.price,
+      imageUrl: this.imageUrl,
+      category: newCategory,
+    });
+  }
+}
