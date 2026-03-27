c8588fe472daea95f52d85e47d1e2fc2145a7541
Romeo-Martial <104857205+Romeo-Martial@users.noreply.github.com>
2026-03-27 10:27:14 +0100

    Add the file CatalogContext.jsx to the code base
    the file implements the catalog provider and a hook to get acces to the public API

---
+++ src/features/catalog/ui/context/CatalogContext.jsx
@@ -0,0 +1,67 @@
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useMemo,
+  useState,
+  useCallback,
+} from "react";
+
+const CatalogContext = createContext(null);
+
+export function CatalogProvider({ children, getProducts }) {
+  if (!getProducts) {
+    throw new Error("CatalogProvider requires getProducts use case");
+  }
+
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const loadProducts = useCallback(async () => {
+    setIsLoading(true);
+    setError(null);
+
+    try {
+      const result = await getProducts.execute();
+
+      if (result.isFailure()) {
+        setError(result.error);
+        return result;
+      }
+
+      setError(null);
+      setProducts(result.value);
+      return result;
+    } finally {
+      setIsLoading(false);
+    }
+  }, [getProducts]);
+
+  useEffect(() => {
+    loadProducts();
+  }, [loadProducts]);
+
+  const value = useMemo(() => {
+    return {
+      products,
+      isLoading,
+      error,
+      loadProducts,
+    };
+  }, [products, isLoading, error, loadProducts]);
+
+  return (
+    <CatalogContext.Provider value={value}>{children}</CatalogContext.Provider>
+  );
+}
+
+export function useCatalogContext() {
+  const context = useContext(CatalogContext);
+
+  if (!context) {
+    throw new Error("useCatalogContext must be used within a CatalogProvider");
+  }
+
+  return context;
+}
