791dbabed2170267e5c71fc330675fc23eaa0c38
Romeo-Martial <104857205+Romeo-Martial@users.noreply.github.com>
2026-03-27 11:34:51 +0100

    Add the file cartLine.js and cart.js to the code base
    the files implement respectively the classes CartLine and CartLine
    which enforce the rule of a cart line and a cart

---
+++ src/features/cart/domain/cart.js
@@ -0,0 +1,80 @@
+import { CartId } from "./valueObjects/cartId.js";
+import { CartLine } from "./cartLine.js";
+import { Money } from "../../_shared/domain/money.js";
+
+export class Cart {
+  constructor(id, lines) {
+    if (!(id instanceof CartId)) {
+      throw new Error("id must be of type CartId");
+    }
+    lines.forEach((line) => {
+      if (!(line instanceof CartLine)) {
+        throw new Error("lines must be an array of CartLine");
+      }
+    });
+    const seen = new Set();
+
+    for (const line of lines) {
+      const key = line.sku.value;
+      if (seen.has(key)) {
+        throw new Error("Duplicate SKU in cart");
+      }
+      seen.add(key);
+    }
+    this.id = id;
+    this.lines = lines;
+    Object.freeze(this);
+  }
+
+  addItem(sku, quantity, unitPrice) {
+    const existing = this.lines.find((line) => line.sku.equals(sku));
+
+    if (existing) {
+      const newQuantity = existing.quantity.add(quantity);
+
+      const newLines = this.lines.map((line) =>
+        line.sku.equals(sku) ? new CartLine(sku, newQuantity, unitPrice) : line,
+      );
+
+      return new Cart(this.id, newLines);
+    }
+
+    const newLines = [...this.lines, new CartLine(sku, quantity, unitPrice)];
+
+    return new Cart(this.id, newLines);
+  }
+
+  removeItem(sku) {
+    const newLines = this.lines.filter((line) => !line.sku.equals(sku));
+
+    return new Cart(this.id, newLines);
+  }
+
+  changeItemQuantity(sku, quantity) {
+    const newLines = this.lines.map((line) =>
+      line.sku.equals(sku) ? line.changeQuantity(quantity) : line,
+    );
+
+    return new Cart(this.id, newLines);
+  }
+
+  hasItem(sku) {
+    const skus = this.lines.map((elt) => elt.sku);
+    return skus.includes(sku);
+  }
+
+  getSubtotal() {
+    if (this.lines.length === 0) {
+      return new Money(0, "EUR");
+    }
+
+    return this.lines.reduce(
+      (total, line) => total.add(line.getSubtotal()),
+      new Money(0, this.lines[0].unitPrice.currency),
+    );
+  }
+
+  getTotalItems() {
+    return this.lines.reduce((total, line) => total + line.quantity.value, 0);
+  }
+}
---
+++ src/features/cart/domain/cartLine.js
@@ -0,0 +1,36 @@
+import { Money } from "../../_shared/domain/money.js";
+import { Quantity } from "./valueObjects/quantity.js";
+import { Sku } from "./valueObjects/sku.js";
+
+export class CartLine {
+  constructor(sku, quantity, unitPrice) {
+    if (!(sku instanceof Sku)) {
+      throw new Error("sku should be of type Sku");
+    }
+    if (!(quantity instanceof Quantity)) {
+      throw new Error("quantity should be of type Quantity");
+    }
+    if (!(unitPrice instanceof Money)) {
+      throw new Error("initPrice should be of type Money");
+    }
+    this.sku = sku;
+    this.quantity = quantity;
+    this.unitPrice = unitPrice;
+    Object.freeze(this);
+  }
+
+  getSubtotal() {
+    return this.unitPrice.multiply(this.quantity);
+  }
+
+  sameSku(otherSku) {
+    return this.sku.equals(otherSku);
+  }
+
+  changeQuantity(newQuantity) {
+    if (!(newQuantity instanceof Quantity)) {
+      throw new Error("Argument must be of type Quantity");
+    }
+    return new CartLine(this.sku, newQuantity, this.unitPrice);
+  }
+}
