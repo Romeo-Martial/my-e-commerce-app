5df65e3c1a003aff9c9f5ba5b0adf5e1ac3fc597
Romeo-Martial <104857205+Romeo-Martial@users.noreply.github.com>
2026-03-28 00:50:59 +0100

    Add the file orderLine.js to the code base
    the file implements the class OrderLine whih enforces
    the order line rules

---
+++ src/features/order/domain/orderLine.js
@@ -0,0 +1,33 @@
+import { Sku } from "../../cart/domain/valueObjects/sku";
+import { Quantity } from "../../cart/domain/valueObjects/quantity";
+import { Money } from "../../_shared/domain/money";
+
+export class OrderLine {
+  constructor({ sku, quantity, unitPrice }) {
+    if (!(sku instanceof Sku)) {
+      throw new Error("sku must be of type Sku");
+    }
+
+    if (!(quantity instanceof Quantity)) {
+      throw new Error("quantity must be of type Quantity");
+    }
+
+    if (!(unitPrice instanceof Money)) {
+      throw new Error("unitPrice must be of type Money");
+    }
+
+    this.sku = sku;
+    this.quantity = quantity;
+    this.unitPrice = unitPrice;
+
+    Object.freeze(this);
+  }
+
+  getSubtotal() {
+    return this.unitPrice.multiply(this.quantity);
+  }
+
+  sameSku(otherSku) {
+    return this.sku.equals(otherSku);
+  }
+}
